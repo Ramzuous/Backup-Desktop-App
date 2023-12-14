807a4e94dc69aa50f3232bf4df9069c0f109a8be
root <root@Laptop-PD-Backup>
2023-12-14 02:10:36 +0100

    First push

---
+++ main.py
@@ -0,0 +1,57 @@
+import tkinter as tk
+
+class Window:
+
+    def __init__(self, appName, settingsName, runBackupName):
+    
+        self.root = tk.Tk()
+        self.root.title(appName)
+        
+        self.root.geometry("250x100")
+
+        self.root.resizable(0,0)
+
+        #inputLabel = tk.Label(self, text=lableInputName)
+        #inputLabel.pack()
+
+        #operationPath = tk.Entry(self)
+        #operationPath.pack()
+
+        #inputExtensionLabel = tk.Label(self, text=labelInputExtension)
+        #inputExtensionLabel.pack()
+        
+        #inputExtension = tk.Entry(self)
+        #inputExtension.pack()
+        
+        #outputExtensionLabel = tk.Label(self, text=labelOutputExtension)
+        #outputExtensionLabel.pack()
+        
+        #outputExtension = tk.Entry(self)
+        #outputExtension.pack()
+
+        self.settings_button = tk.Button(text=settingsName, command=self.openSettings)
+        self.settings_button.pack(pady=10)
+
+        self.runBackup_button = tk.Button(text=runBackupName, command=self.runBackup)
+        self.runBackup_button.pack(pady=10)
+
+        self.root.mainloop()
+
+    def openSettings(self):
+        print("Open Settings")
+        
+        
+    def runBackup(self):
+        print("Run Backup")
+            
+
+# Variables
+
+appName = "Backup"
+
+settingsName = "Settings"
+
+runBackupName = "Run Backup"
+
+###############################            
+w = Window(appName, settingsName, runBackupName)
